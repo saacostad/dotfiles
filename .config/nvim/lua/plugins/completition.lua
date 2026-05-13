b82b8cd47e6e635731b18bac17c012c580d702e5
morumotto <92110089+saacostad@users.noreply.github.com>
2026-05-12 22:13:29 -0500

    Update completition.lua

--- .config/nvim/lua/plugins/completition.lua
+++ .config/nvim/lua/plugins/completition.lua
@@ -1,66 +1,70 @@
 -- ~/.config/nvim/lua/plugins/completion.lua
-
 return {
+   {
+    "hrsh7th/cmp-nvim-lsp"
+  },
   {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    "L3MON4D3/LuaSnip",
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
     },
+  },
+  {
+    "hrsh7th/nvim-cmp",
     config = function()
-      local cmp     = require("cmp")
-      local luasnip = require("luasnip")
-
+      local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
           expand = function(args)
-            luasnip.lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
-          completion    = cmp.config.window.bordered(),
+          completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"]     = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"]     = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"]     = cmp.mapping.abort(),
-          ["<CR>"]      = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"]     = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"]   = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" },
+          { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
-          { name = "path" },
         }),
       })
     end,
   },
+	
+{
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  lazy = false,
+  config = function()
+  require("todo-comments").setup({
+   highlight = {
+    comments_only = false,
+    before  = "fg",
+    keyword = "fg",
+    after   = "fg",
+  },
+  colors = {
+    error   = { "#f38ba8" }, -- Red
+    warning = { "#fab387" }, -- Peach
+    info    = { "#89b4fa" }, -- Blue
+    hint    = { "#94e2d5" }, -- Teal
+    default = { "#cba6f7" }, -- Mauve
+    test    = { "#a6e3a1" }, -- Green
+  },
+  })
+  vim.cmd([[highlight clear Todo]])
+  end,
+}
 }
