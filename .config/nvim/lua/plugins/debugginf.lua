1b822365f7f2b43e0df2d41d1f147b83f601ca13
morumotto <92110089+saacostad@users.noreply.github.com>
2026-05-12 22:15:54 -0500

    Create debugginf.lua

---
+++ .config/nvim/lua/plugins/debugginf.lua
@@ -0,0 +1,108 @@
+return {
+	"mfussenegger/nvim-dap",
+
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"rcarriga/nvim-dap-ui", 
+	},
+
+	config = function ()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		dapui.setup()
+
+		-- DEBUGGER UI
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- DEBUGS KEYBINDINGS
+		vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
+		vim.keymap.set('n', '<Leader>dc', function() require('dap').continue() end)
+
+		-- DEBUGGERS FOR C AND C++
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb", -- or if not in $PATH: "/absolute/path/to/codelldb"
+
+			-- On windows you may have to uncomment this:
+			-- detached = false,
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				cwd = '${workspaceFolder}',
+				stopOnEntry = false,
+			},
+		}
+		dap.configurations.c = dap.configurations.cpp
+		dap.configurations.rust = dap.configurations.cpp
+
+		-- DEBUGGER FOR PYTHON
+		dap.adapters.python = function(cb, config)
+			if config.request == 'attach' then
+				---@diagnostic disable-next-line: undefined-field
+				local port = (config.connect or config).port
+				---@diagnostic disable-next-line: undefined-field
+				local host = (config.connect or config).host or '127.0.0.1'
+				cb({
+					type = 'server',
+					port = assert(port, '`connect.port` is required for a python `attach` configuration'),
+					host = host,
+					options = {
+						source_filetype = 'python',
+					},
+				})
+			else
+				cb({
+					type = 'executable',
+					command = os.getenv("VIRTUAL_ENV") .. "/bin/python",
+					args = { '-m', 'debugpy.adapter' },
+					options = {
+						source_filetype = 'python',
+					},
+				})
+			end
+		end
+		dap.configurations.python = {
+			{
+				-- The first three options are required by nvim-dap
+				type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+				request = 'launch';
+				name = "Launch file";
+
+				-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+				program = "${file}"; -- This configuration will launch the current file if used.
+				pythonPath = function()
+					-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+					-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+					-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+					local cwd = vim.fn.getcwd()
+					if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+						return cwd .. '/venv/bin/python'
+					elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+						return cwd .. '/.venv/bin/python'
+					else
+						return os.getenv("VIRTUAL_ENV") .. "/bin/python" 
+					end
+				end;
+			},
+		}
+	end
+}
